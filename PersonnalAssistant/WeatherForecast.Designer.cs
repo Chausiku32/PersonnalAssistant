1ce0ee6bfb36c6f1690a0fe5941164eb137f13d4
Brian Phiri <Brian Phiri>
2016-06-20 16:56:05 +0300

    Nick's changes

---
+++ PersonnalAssistant/WeatherForecast.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace PersonnalAssistant
+{
+    partial class WeatherForecast
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WeatherForecast
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 427);
+            this.Name = "WeatherForecast";
+            this.Text = "WeatherForecast";
+            this.Load += new System.EventHandler(this.WeatherForecast_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ PersonnalAssistant/WeatherForecast.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PersonnalAssistant
+{
+    class WeatherForecast : Form1
+    {
+
+        string temp, condition;
+
+        public WeatherForecast()
+        {
+            InitializeComponent();
+        }
+
+        private void WeatherForecast_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public String GetWeather(String input)
+        {
+            try
+            {
+                String query = String.Format("https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='Nairobi, state')&format=xml&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys");
+                XmlDocument wData = new XmlDocument();
+                wData.Load(query);
+
+                XmlNamespaceManager manager = new XmlNamespaceManager(wData.NameTable);
+                manager.AddNamespace("yweather", "http://xml.weather.yahoo.com/ns/rss/1.0");
+
+                XmlNode channel = wData.SelectSingleNode("query").SelectSingleNode("results").SelectSingleNode("channel");
+                XmlNodeList nodes = wData.SelectNodes("query/results/channel");
+                try
+                {
+                    temp = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["temp"].Value;
+                    condition = channel.SelectSingleNode("item").SelectSingleNode("yweather:condition", manager).Attributes["text"].Value;
+
+                    if (input == "temp")
+                    {
+                        return temp;
+                    }
+                    if (input == "cond")
+                    {
+                        return condition;
+                    }
+                }
+                catch
+                {
+                    return "Error Reciving data";
+                }
+                return "error";
+            }
+            catch (System.Xml.XmlException)
+            {
+                return "s";
+            }
+
+        }
+
+    }
+}
